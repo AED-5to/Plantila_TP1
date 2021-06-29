5ee578d526a0c4628e8de1c3409f9b3d187abdbc
Nicolás Ferragamo <nferragamo@frba.utn.edu.ar>
2021-06-29 10:07:43 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <util/delay.h>
+
+/*
+
+Respetar este pin out.
+
+PD4 -> BOTON 1 
+PD5 -> BOTON 2 
+PD6 -> BOTON 3 
+PD7 -> BOTON 4
+
+
+PB0 -> LED 1 
+PB1 -> LED 2 
+PB2 -> LED 3 
+PB3 -> LED 4
+
+*/
+
+
+int main (void)
+{
+ 
+  //! aca se escriben las inicializaciones
+
+  while(1)
+  {
+   //! aca se escribe el programa principal
+  }
+
+  return 0;
+}
